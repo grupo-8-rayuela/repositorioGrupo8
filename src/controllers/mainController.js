d229ab71a7a7dd63691519537b4aaf291eff02de
pablosimkievich <pablosimkievich@gmail.com>
2022-07-06 20:33:03 -0300

    contenido dinamico home, formulario POST sin guardar en database

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,39 @@
 const mainController = {
     index : (req, res) => {
-        res.render('home'); // home ejs
+        let arrayMock = [
+            {
+                nombre: "Banco didáctico",
+                precio: "$ 4800",
+                cuotas: "3 cuotas de $1600"
+            },
+            {
+                nombre: "Cocodrilo de arrastre",
+                precio: "$ 3200",
+                cuotas: "3 cuotas de $1066"
+            },
+            {
+                nombre: "Cohete multifunción",
+                precio: "$ 7900",
+                cuotas: "3 cuotas de $2630"
+            },
+            {
+                nombre: "Mesa de expresión",
+                precio: "$ 4500",
+                cuotas: "3 cuotas de $1500"
+            },
+            {
+                nombre: "Torre geométrica",
+                precio: "$ 1600",
+                cuotas: "3 cuotas de $530"
+            },
+            {
+                nombre: "Carro andador",
+                precio: "$ 2500",
+                cuotas: "3 cuotas de $830"
+            }
+           ]
+
+        res.render('home', {arrayMock, arrayMock}); // home ejs
     }
 }
 
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const dataPath = path.join(__dirname, '../database/juguetes.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+
 const productController = {
     producto : (req, res) => {
         res.render('productDetail')   // productDetail.ejs
@@ -12,7 +18,13 @@ const productController = {
         res.render('crearProducto')  // crearProducto SKU
     },
     guardar : (req, res) => {
-
+        let newProduct = req.body
+        
+        console.log(req.body)
+        data.push(newProduct);
+        fs.writeFileSync(dataPath, JSON.stringify(data, null, ' ')) 
+        console.log(newProduct) 
+        res.redirect('/')
     },
     guardarSKU : (req, res) => {
         
